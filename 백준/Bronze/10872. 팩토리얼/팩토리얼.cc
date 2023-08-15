74bf71bfde18642b556af384a608da8b57b3df8b
Park Geonho <73037262+pagh2322@users.noreply.github.com>
2023-08-15 21:05:53 +0900

    [Bronze V] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int num) {
+    if (num == 0) return 1;
+    return num * factorial(num - 1);
+}
+
+int main()
+{
+    int N; cin >> N;
+    cout << factorial(N);
+	return 0;
+}
