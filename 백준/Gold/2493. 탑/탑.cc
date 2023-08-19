20a83a21425aee88bee86a9840794537cb42eb73
Park Geonho <73037262+pagh2322@users.noreply.github.com>
2023-08-19 15:35:46 +0900

    [Gold V] Title: 탑, Time: 104 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -5,25 +5,27 @@ using namespace std;
 
 int main()
 {    
-    int N; scanf("%d", &N);
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    int N; cin >> N;
     stack<pair<int, int> > st;
 
     for (int i = 1; i <= N; i++)
     {
-        int num; scanf("%d", &num);
+        int num; cin >> num;
 
         while (!st.empty())
         {
             if (st.top().second > num)
             {
-                printf("%d ", st.top().first);
+                cout << st.top().first << " ";
                 break;
             }
             st.pop();
         }
         if (st.empty())
         {
-            printf("0 ");
+            cout << "0 ";
         }
         st.push({i, num});
     }
