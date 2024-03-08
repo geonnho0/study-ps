3ab4d7496bc576589447092f16b51c4f9967dddd
Park Geonho <73037262+pagh2322@users.noreply.github.com>
2024-03-08 14:45:04 +0800

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 96 ms, Memory: 12104 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] line = br.readLine().split(" ");
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(line[i]);
+        }
+
+        int ans = 0;
+        int[] dp = new int[N];
+        for (int i = 0; i < N; i++) {
+            dp[i] = A[i];
+            for (int j = 0; j < i; j++) {
+                if (A[j] < A[i] && dp[i] < dp[j] + A[i]) {
+                    dp[i] = dp[j] + A[i];
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        System.out.print(ans);
+    }
+
+}
