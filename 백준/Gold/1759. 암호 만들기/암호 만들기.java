1ed3e38651ef1c1d467aa509e2aa9ae7681f2311
Park Geonho <73037262+pagh2322@users.noreply.github.com>
2024-06-17 23:29:31 +0800

    [Gold V] Title: 암호 만들기, Time: 104 ms, Memory: 18780 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,89 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    static boolean[] visited;
     static int L, C;
-    static char[] chars;
-    static char[] password;
-    static StringBuilder sb = new StringBuilder();
-    static ArrayList<String> ans = new ArrayList<>();
+    static String[] alphabets;
+    static boolean[] visited;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        L = scanner.nextInt();
-        C = scanner.nextInt();
-        scanner.nextLine();
-        chars = new char[C];
-        password = new char[L];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        L = Integer.parseInt(line[0]);
+        C = Integer.parseInt(line[1]);
+        alphabets = new String[C];
+        line = br.readLine().split(" ");
+        for (int i = 0; i < C; i++)
+            alphabets[i] = line[i];
+
+        Arrays.sort(alphabets);
         visited = new boolean[C];
-        String[] line = scanner.nextLine().split(" ");
-        for (int i = 0; i < C; i++) {
-            chars[i] = line[i].charAt(0);
-        }
-        dfs(0);
-        Collections.sort(ans);
-        for (String a : ans) {
-            sb.append(a).append("\n");
-        }
-        System.out.print(sb);
+        dfs(0, new ArrayList<>());
     }
 
-    private static void dfs(int length) {
-        if (length == L) {
-            if (isValid()) {
-                String str = "";
-                for (char c: password) {
-                    str += c;
-                }
-                ans.add(str);
-            }
+    static void dfs(int index, List<String> password) {
+        if (password.size() == L && check(password)) {
+            StringBuilder sb = new StringBuilder();
+            for (String p : password)
+                sb.append(p);
+            System.out.println(sb);
             return;
         }
-        for (int i = 0; i < C; i++) {
-            if (visited[i]) {
-                continue;
-            }
+        for (int i = index; i < C; i++) {
+            if (visited[i]) continue;
+
             visited[i] = true;
-            password[length] = chars[i];
-            if (isSorted(length + 1)) {
-                dfs(length + 1);
-            }
+            List<String> newPass = new ArrayList<>(password);
+            newPass.add(alphabets[i]);
+            dfs(i + 1, newPass);
             visited[i] = false;
         }
     }
 
-    private static boolean isSorted(int length) {
-        char curr = password[0];
-        for (int i = 1; i < length; i++) {
-            if (curr > password[i]) {
-                return false;
-            }
-            curr = password[i];
-        }
-        return true;
-    }
-
-    private static boolean isValid() {
-        char[] arr = {'a', 'e', 'i', 'o', 'u'};
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (contains(arr[i])) {
-                count++;
-            }
-        }
-        if (count < 1 || L - count < 2) {
+    static boolean check(List<String> password) {
+        if (password.size() < 3)
             return false;
-        }
-        return isSorted(L);
-    }
-
-    private static boolean contains(char c) {
-        for (char p : password) {
-            if (p == c) {
-                return true;
-            }
-        }
+        int count = 0;
+        if (password.contains("a")) count++;
+        if (password.contains("e")) count++;
+        if (password.contains("i")) count++;
+        if (password.contains("o")) count++;
+        if (password.contains("u")) count++;
+        if (count == 0) return false;
+        if (password.size() - count >= 2) return true;
         return false;
     }
 
