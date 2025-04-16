b973708898d0ef1ebb9c3f7ae8e54ecbdaf63041
Park Geonho <73037262+geonnho0@users.noreply.github.com>
2025-04-16 22:39:14 +0900

    [level 2] Title: 가장 큰 수, Time: 273.88 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,24 +2,12 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        PriorityQueue<String> q = new PriorityQueue<>((s1, s2) -> s2.compareTo(s1));
-        q.offer("B");
-        q.offer("A");
-        q.offer("C");
-        while (!q.isEmpty()) {
-            System.out.println(q.poll());
-        }
-        int length = numbers.length;
-        String[] numberStrings = new String[length];
-        for (int i = 0; i < length; i++)
-            numberStrings[i] = String.valueOf(numbers[i]);
-                                              
-        Arrays.sort(numberStrings, (str1, str2) -> (str2 + str1).compareTo(str1 + str2));
-        
-        if (numberStrings[0].equals("0")) return "0";
-        
+        String[] strs = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
+        Arrays.sort(strs, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+        if (strs[0].equals("0")) return "0";
         StringBuilder sb = new StringBuilder();
-        for (String s: numberStrings) sb.append(s);
+        for (String s : strs)
+            sb.append(s);
         return sb.toString();
     }
 }
