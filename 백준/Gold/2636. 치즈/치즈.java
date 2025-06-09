0d333fb3668716195afb65425928ae455e6b8ff6
Park Geonho <73037262+geonnho0@users.noreply.github.com>
2025-06-09 22:54:45 +0900

    [Gold IV] Title: 치즈, Time: 92 ms, Memory: 13792 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,70 +1,79 @@
 import java.io.*;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-
-    static boolean[][] melted;
-    static int[][] graph;
-    static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
-    static int N, M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] line = br.readLine().split(" ");
-        N = Integer.parseInt(line[0]);
-        M = Integer.parseInt(line[1]);
-        graph = new int[N][M];
-        melted = new boolean[N][M];
-        int cheeseCount = 0;
+        int N = Integer.parseInt(line[0]);
+        int M = Integer.parseInt(line[1]);
+        int[][] board = new int[N][M];
         for (int i = 0; i < N; i++) {
             line = br.readLine().split(" ");
-            for (int j = 0; j < M; j++) {
-                graph[i][j] = Integer.parseInt(line[j]);
-                if (graph[i][j] == 1)
-                    cheeseCount++;
-            }
+            for (int j = 0; j < M; j++)
+                board[i][j] = Integer.parseInt(line[j]);
         }
+        int[] answer = solution(N, M, board);
+        System.out.println(answer[0] + "\n" + answer[1]);
+    }
+
+    private static int[][] board;
+    private static int N, M;
+    private static final int[] dx = {0, 0, -1, 1};
+    private static final int[] dy = {-1, 1, 0, 0};
+
+    private static int[] solution(int n, int m, int[][] boards) {
+        N = n;
+        M = m;
+        board = boards;
+
+        int totalCheeseCount = 0;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
+                if (board[i][j] == 1)
+                    totalCheeseCount++;
+
         int time = 0;
         while (true) {
-            time++;
-            List<int[]> cheese = bfs();
-            cheeseCount -= cheese.size();
-            if (cheeseCount == 0) {
-                System.out.println(time);
-                System.out.print(cheese.size());
+            int cheeseCount = countCheeseForMelting();
+
+            if (totalCheeseCount == cheeseCount)
                 break;
-            }
-            for (int[] c : cheese) {
-                graph[c[0]][c[1]] = 0;
-                melted[c[0]][c[1]] = true;
-            }
+
+            totalCheeseCount -= cheeseCount;
+            time++;
         }
+        return new int[]{time + 1, totalCheeseCount};
     }
 
-    static List<int[]> bfs() {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0, 0});
+    private static int countCheeseForMelting() {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0});
         boolean[][] visited = new boolean[N][M];
-        List<int[]> cheese = new ArrayList<>();
+        visited[0][0] = true;
 
-        while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
+        int count = 0;
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+
             for (int i = 0; i < 4; i++) {
-                int nextX = curr[0] + dx[i], nextY = curr[1] + dy[i];
-                if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= M)
+                int nx = curr[0] + dx[i];
+                int ny = curr[1] + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny])
                     continue;
-                if (visited[nextX][nextY])
-                    continue;
-                if (graph[nextX][nextY] == 0) {
-                    visited[nextX][nextY] = true;
-                    queue.offer(new int[]{nextX, nextY});
+
+                visited[nx][ny] = true;
+                if (board[nx][ny] == 0) {
+                    q.offer(new int[]{nx, ny});
                     continue;
                 }
-                visited[nextX][nextY] = true;
-                cheese.add(new int[]{nextX, nextY});
+                board[nx][ny] = 0;
+                count++;
             }
         }
-        return cheese;
+        return count;
     }
-
 }
