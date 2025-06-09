4dbdb9530f69efa5949bfc8841cbdcb4c8aa1ce1
Park Geonho <73037262+geonnho0@users.noreply.github.com>
2025-06-09 23:47:11 +0900

    [level 3] Title: 여행경로, Time: 12.56 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,38 +1,43 @@
 import java.util.*;
 
 class Solution {
-    
     boolean[] visited;
-    List<String> answers = new ArrayList<>();
     String[][] tickets;
-    int maxLevel;
-    
+    int totalCount;
+    String answer = "";
     public String[] solution(String[][] tickets) {
-        init(tickets);
-        dfs("ICN", "ICN", 0);
-        Collections.sort(answers);
-        String[] answer = answers.get(0).split(" ");
-        return answer;
-    }
-    
-    void init(String[][] tickets) {
-        maxLevel = tickets.length;
+        totalCount = tickets.length;
+        visited = new boolean[totalCount];
+        Arrays.sort(tickets, (t1, t2) -> {
+            if (t1[0].equals(t2[0]))
+                return t1[1].compareTo(t2[1]);
+            return t1[0].compareTo(t2[1]);
+        });
         this.tickets = tickets;
-        visited = new boolean[maxLevel];
+        
+        dfs("ICN", ("ICN"), 0);
+        return answer.split(" ");
     }
     
-    void dfs(String start, String route, int level) {
-        if (level == maxLevel) {
-            answers.add(route);
+    void dfs(String current, String route, int visitedCount) {
+        if (!answer.equals(""))
+            return;
+
+        if (visitedCount == totalCount) {
+            answer = route;
             return;
         }
-        for (int i = 0; i < maxLevel; i++) {
-            if (start.equals(tickets[i][0]) && !visited[i]) {
-                visited[i] = true;
-                dfs(tickets[i][1], route + " " + tickets[i][1], level + 1);
-                visited[i] = false;
-            }
+        
+        for (int i = 0; i < tickets.length; i++) {
+            if (visited[i])
+                continue;
+            
+            if (!current.equals(tickets[i][0]))
+                continue;
+            
+            visited[i] = true;
+            dfs(tickets[i][1], route + " " + tickets[i][1], visitedCount + 1);
+            visited[i] = false;
         }
     }
-    
 }
