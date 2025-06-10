bbbb2272e491be7e5b307960721df028f6d46834
Park Geonho <73037262+geonnho0@users.noreply.github.com>
2025-06-10 23:29:22 +0900

    [level 3] Title: 순위, Time: 10.49 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,41 +1,42 @@
-import java.util.*;
-
 class Solution {
-    
-    int[][] graph;
-    int n;
-    
     public int solution(int n, int[][] results) {
-        init(n, results);
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (graph[i][j] == 0 && graph[i][k] == 1 && graph[k][j] == 1)
-                        graph[i][j] = 1;
-                }
-            }
-        }
-        
         int answer = 0;
+        int[][] graph = new int[n + 1][n + 1];
+        for (int[] r : results) {
+            graph[r[0]][r[1]] = 1;
+            graph[r[1]][r[0]] = -1;
+        }
+        floyd(graph);
+        
         for (int i = 1; i <= n; i++) {
             int count = 0;
-            for (int j = 1; j <= n; j++) {
-                if (graph[i][j] == 1 || graph[j][i] == 1)
+            for (int result : graph[i])
+                if (result != 0)
                     count++;
-            }
+            
             if (count == n - 1)
                 answer++;
         }
         return answer;
     }
     
-    void init(int n, int[][] res) {
-        this.n = n;
-        graph = new int[n + 1][n + 1];
-        for (int[] r : res) {
-            int start = r[0], end = r[1];
-            graph[start][end] = 1;
+    public void floyd(int[][] graph) {
+        int n = graph.length;
+        for (int k = 1; k < n; k++) {
+            for (int i = 1; i < n; i++) {
+                for (int j = 1; j < n; j++) {
+                    if (graph[i][j] != 0)
+                        continue;
+                    
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1;
+                        graph[j][i] = -1;
+                    } else if (graph[i][k] == -1 && graph[k][j] == -1) {
+                        graph[i][j] = -1;
+                        graph[j][i] = 1;
+                    }
+                }
+            }
         }
     }
-    
 }
