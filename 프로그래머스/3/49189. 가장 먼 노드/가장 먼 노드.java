05acd7c360356e37533d3a19fd5a1027fb56407f
Park Geonho <73037262+geonnho0@users.noreply.github.com>
2025-06-10 23:05:20 +0900

    [level 3] Title: 가장 먼 노드, Time: 53.81 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,51 +1,45 @@
 import java.util.*;
 
 class Solution {
-    
-    ArrayList<Integer>[] edges;
-    int[] distances;
-    
+    List<Integer>[] edges;
+    int n;
     public int solution(int n, int[][] edge) {
-        int answer = 0;
+        this.n = n;
         edges = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i <= n; i++)
             edges[i] = new ArrayList<>();
+        for (int[] e : edge) {
+            edges[e[0]].add(e[1]);
+            edges[e[1]].add(e[0]);
         }
-        for (int i = 0; i < edge.length; i++) {
-            edges[edge[i][0]].add(edge[i][1]);
-            edges[edge[i][1]].add(edge[i][0]);
-        }
-        distances = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            distances[i] = Integer.MAX_VALUE;
-        }
-        bfs();
-        int max = 0;
-        for (int i = 1; i <= n; i++) {
-            max = Math.max(max, distances[i]);
-        }
-        for (int i = 1; i <= n; i++) {
-            if (max == distances[i]) {
-                answer++;
-            }
-        }
-        return answer;
+        return bfs(1);
     }
     
-    private void bfs() {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(1);
-        distances[1] = 0;
+    int bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        int[] dist = new int[n + 1];
+        dist[start] = 1;
         
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
+        while (!q.isEmpty()) {
+            Integer curr = q.poll();
             
-            for (int next: edges[curr]) {
-                if (distances[next] > distances[curr] + 1) {
-                    distances[next] = distances[curr] + 1;
-                    queue.offer(next);
+            for (Integer next : edges[curr]) {
+                if (dist[next] == 0) {
+                    dist[next] = dist[curr] + 1;
+                    q.offer(next);
                 }
             }
         }
+        
+        int max = 0;
+        for (int d : dist)
+            if (d > max)
+                max = d;
+        int count = 0;
+        for (int d : dist)
+            if (d == max)
+                count++;
+        return count;
     }
 }
