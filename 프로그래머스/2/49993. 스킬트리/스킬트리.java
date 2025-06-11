d7713c8bcdc0aeb79eb9b36b602c8dd4f1f5a794
Park Geonho <73037262+geonnho0@users.noreply.github.com>
2025-06-11 21:42:15 +0900

    [level 2] Title: 스킬트리, Time: 0.22 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,28 +1,32 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(String skill, String[] skill_trees) {
-        int[] sequence = new int[26];
-        for (int i = 0; i < skill.length(); i++)
-            sequence[skill.charAt(i) - 'A'] = i + 1;
+        char[] arr = skill.toCharArray();
+        Map<Character, Integer> indexMap = new HashMap<>();
+        int index = 0;
+        for (char c : arr)
+            indexMap.put(c, index++);
+        
         int answer = 0;
-        for (int i = 0; i < skill_trees.length; i++) {
-            String curr = skill_trees[i];
-            int index = 0;
-            boolean flag = true;
-            for (int j = 0; j < curr.length() && flag; j++) {
-                int seq = sequence[curr.charAt(j) - 'A'];
-                if (seq == 0)
-                    continue;
-                if (seq - index != 1)
-                    flag = false;
-                index++;
-            }
-            if (flag)
+        for (String s : skill_trees) {
+            if (validate(s, indexMap))
                 answer++;
         }
         return answer;
     }
     
+    boolean validate(String tree, Map<Character, Integer> indexMap) {
+        char[] arr = tree.toCharArray();
+        int curr = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (indexMap.get(arr[i]) == null)
+                continue;
+            int index = indexMap.get(arr[i]);
+            if (index != curr)
+                return false;
+            curr++;
+        }
+        return true;
+    }
 }
