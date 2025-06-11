a55720f275731faa231439753a105aeac0aa80e3
Park Geonho <73037262+geonnho0@users.noreply.github.com>
2025-06-11 22:10:41 +0900

    [level 3] Title: 부대복귀, Time: 111.66 ms, Memory: 173 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -12,38 +12,33 @@ class Solution {
             edges[r[1]].add(r[0]);
         }
         
+        int[] dist = findShortest(destination, n);
+        
         int[] answer = new int[sources.length];
-        for (int i = 0; i < sources.length; i++) {
-            if (sources[i] == destination) {
-                answer[i] = 0;
-                continue;
-            }
-            answer[i] = findShortest(sources[i], destination, n);
-        }
+        for (int i = 0; i < sources.length; i++)
+            answer[i] = dist[sources[i]];
         
         return answer;
     }
     
-    int findShortest(int start, int dest, int n) {
+    int[] findShortest(int start, int n) {
         Queue<Integer> q = new LinkedList<>();
         q.offer(start);
         int[] dist = new int[n + 1];
-        dist[start] = 1;
+        Arrays.fill(dist, -1);
+        dist[start] = 0;
         
         while (!q.isEmpty()) {
             int curr = q.poll();
             
-            
             for (int next : edges[curr]) {
-                if (dist[next] != 0)
+                if (dist[next] != -1)
                     continue;
-                if (next == dest)
-                    return dist[curr];
                 
                 dist[next] = dist[curr] + 1;
                 q.offer(next);
             }
         }
-        return -1;
+        return dist;
     }
 }
