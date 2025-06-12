57ab81d4eb92ea554b93c63775c88ffa97fb6e18
Park Geonho <73037262+geonnho0@users.noreply.github.com>
2025-06-12 23:47:53 +0900

    [level 2] Title: 디펜스 게임, Time: 132.80 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -2,19 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int k, int[] enemy) {
-        Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> defendedCount = new PriorityQueue<>(Collections.reverseOrder());
+        
+        int answer = 0;
         for (int i = 0; i < enemy.length; i++) {
-            n -= enemy[i];
-            queue.offer(enemy[i]);
+            int e = enemy[i];
+            n -= e;
+            defendedCount.offer(e);
+            if (n >= 0)
+                continue;
             
-            if (n >= 0) {
-                continue;
-            }
-            if (k > 0 && !queue.isEmpty()) {
-                n += queue.poll();
+            if (k > 0 && !defendedCount.isEmpty()) {
                 k--;
+                n += defendedCount.poll();
                 continue;
             }
+            
             return i;
         }
         return enemy.length;
