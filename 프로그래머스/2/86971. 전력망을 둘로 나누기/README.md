23a83e932f740c3fe50b93c548f3cb6efd632a82
Park Geonho <73037262+geonnho0@users.noreply.github.com>
2025-06-15 18:43:58 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.77 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=java) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.18 ms
+메모리: 86.5 MB, 시간: 3.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 19일 13:44:23
+2025년 06월 15일 18:43:56
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,52 +1,59 @@
-import java.util.*;
-
 class Solution {
-    
     int[] parent;
-    
+    int n;
+    int[][] wires;
     public int solution(int n, int[][] wires) {
+        this.wires = wires;
+        this.n = n;
         int answer = Integer.MAX_VALUE;
-        int len = wires.length;
-        for (int i = 0; i < len; i++) {
-            init(n);
-            for (int j = 0; j < len; j++) {
-                if (i == j) {
-                    continue;
-                }
-                union(wires[j][0], wires[j][1]);
-            }
-            
-            int count1 = 1, count2 = 0;
-            int temp = find(1);
-            for (int j = 2; j <= n; j++) {
-                if (temp == find(j))
-                    count1++;
-                else
-                    count2++;
-            }
-            answer = Math.min(answer, Math.abs(count1 - count2));
+        for (int i = 0; i < wires.length; i++) {
+            int diff = findDiff(i);
+            if (answer > diff)
+                answer = diff;
         }
-        
         return answer;
     }
     
-    void init(int n) {
+    int findDiff(int exceptWireIndex) {
+        init();
+        
+        for (int i = 0; i < wires.length; i++) {
+            if (i == exceptWireIndex)
+                continue;
+            union(wires[i][0], wires[i][1]);
+        }
+        
+        int[] count = new int[2];
+        int firstRoot = parent[1];
+        count[0] = 1;
+        for (int i = 2; i <= n; i++) {
+            if (findRoot(i) == firstRoot)
+                count[0]++;
+            else
+                count[1]++;
+        }
+        int ret = count[1] - count[0];
+        return ret > 0 ? ret : 0 - ret;
+    }
+    
+    void init() {
         parent = new int[n + 1];
-        for (int i = 1; i < n; i++)
+        for (int i = 0; i <= n; i++)
             parent[i] = i;
     }
     
-    int find(int a) {
-        if (parent[a] == a)
-            return a;
-        return parent[a] = find(parent[a]);
+    int findRoot(int x) {
+        if (x == parent[x])
+            return x;
+        return parent[x] = findRoot(parent[x]);
     }
     
     void union(int a, int b) {
-        int aRoot = find(a), bRoot = find(b);
-        if (aRoot == bRoot)
-            return;
-        parent[bRoot] = aRoot; 
+        int rootA = findRoot(a);
+        int rootB = findRoot(b);
+        if (rootA < rootB)
+            parent[rootB] = rootA;
+        else
+            parent[rootA] = rootB;
     }
-    
 }
