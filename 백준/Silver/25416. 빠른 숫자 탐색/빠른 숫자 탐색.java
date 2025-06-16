2bd5457fe8455c1647d4f66396039763575b8993
Park Geonho <73037262+geonnho0@users.noreply.github.com>
2025-06-16 22:14:48 +0900

    [Silver II] Title: 빠른 숫자 탐색, Time: 68 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Silver/25416. 빠른 숫자 탐색/빠른 숫자 탐색.java
+++ 백준/Silver/25416. 빠른 숫자 탐색/빠른 숫자 탐색.java
@@ -1,51 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int[][] graph = new int[5][5];
+    private static int solution(int r, int c, int[][] board) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{r, c, 0});
+        boolean[][] visited = new boolean[5][5];
+        visited[r][c] = true;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 0; i < 5; i++) {
-            String[] line = br.readLine().split(" ");
-            for (int j = 0; j < 5; j++) {
-                graph[i][j] = Integer.parseInt(line[j]);
-            }
-        }
-        String[] line = br.readLine().split(" ");
-        int r = Integer.parseInt(line[0]), c = Integer.parseInt(line[1]);
-        System.out.print(bfs(r, c));
-    }
+        int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
 
-    static int bfs(int r, int c) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{r, c});
-        int[][] distances = new int[5][5];
-        int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
-        distances[r][c] = 1;
-
-        while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
             for (int i = 0; i < 4; i++) {
-                int nextX = curr[0] + dx[i], nextY = curr[1] + dy[i];
-                if (nextX < 0 || nextX >= 5 || nextY < 0 || nextY >= 5) {
+                int nx = curr[0] + dx[i];
+                int ny = curr[1] + dy[i];
+
+                if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5 || visited[nx][ny] || board[nx][ny] == -1)
                     continue;
-                }
-                if (graph[nextX][nextY] == -1 || distances[nextX][nextY] != 0) {
-                    continue;
-                }
-                distances[nextX][nextY] = distances[curr[0]][curr[1]] + 1;
-                queue.offer(new int[]{nextX, nextY});
-                if (graph[nextX][nextY] == 1) {
-                    return distances[nextX][nextY] - 1;
-                }
+
+                if (board[nx][ny] == 1)
+                    return curr[2] + 1;
+
+                visited[nx][ny] = true;
+                q.offer(new int[]{nx, ny, curr[2] + 1});
             }
         }
         return -1;
     }
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] board = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            String[] line = br.readLine().split(" ");
+            for (int j = 0; j < 5; j++) {
+                board[i][j] = Integer.parseInt(line[j]);
+            }
+        }
+
+        String[] line = br.readLine().split(" ");
+        int r = Integer.parseInt(line[0]);
+        int c = Integer.parseInt(line[1]);
+
+        System.out.println(solution(r, c, board));
+    }
 }
