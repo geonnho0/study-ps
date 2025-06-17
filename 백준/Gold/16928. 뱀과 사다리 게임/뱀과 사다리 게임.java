8f8c60eb679606fdb8aeb3ae269bdd561eb7be72
Park Geonho <73037262+geonnho0@users.noreply.github.com>
2025-06-17 23:52:28 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -1,51 +1,71 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int[] board = new int[101];
+    private static int solution(int N, int M, int[][] ladders, int[][] snakes) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(1);
+        int[] dist = new int[101];
+        Arrays.fill(dist, -1);
+        dist[1] = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int[] l : ladders)
+            map.put(l[0], l[1]);
+        for (int[] s : snakes)
+            map.put(s[0], s[1]);
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+
+            for (int i = 1; i <= 6; i++) {
+                int next = curr + i;
+
+                if (dist[next] != -1) continue;
+
+                dist[next] = dist[curr] + 1;
+
+                if (next == 100)
+                    return dist[next];
+
+                int destination = map.getOrDefault(next, -1);
+
+                if (destination == -1) {
+                    q.offer(next);
+                    continue;
+                }
+
+                if (dist[destination] == -1 || dist[destination] > dist[curr] + 1) {
+                    dist[destination] = dist[curr] + 1;
+                    q.offer(destination);
+                }
+            }
+        }
+
+        return -1;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] line = br.readLine().split(" ");
-        int N = Integer.parseInt(line[0]);
-        int M = Integer.parseInt(line[1]);
-        for (int i = 1; i < 101; i++) {
-            board[i] = i;
-        }
-        for (int i = 0; i < N + M; i++) {
+        int n = Integer.parseInt(line[0]);
+        int m = Integer.parseInt(line[1]);
+
+        int[][] ladders = new int[n][2];
+        for (int i = 0; i < n; i++) {
             line = br.readLine().split(" ");
-            board[Integer.parseInt(line[0])] = Integer.parseInt(line[1]);
+            ladders[i][0] = Integer.parseInt(line[0]);
+            ladders[i][1] = Integer.parseInt(line[1]);
         }
-        System.out.print(bfs());
-    }
 
-    static int bfs() {
-        Queue<Integer> queue = new LinkedList<>();
-        int[] distances = new int[101];
-        queue.offer(1);
-
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-
-            for (int i = 1; i < 7; i++) {
-                int next = curr + i;
-                if (next > 100) {
-                    break;
-                }
-                if (distances[board[next]] == 0) {
-                    distances[board[next]] = distances[curr] + 1;
-                    queue.offer(board[next]);
-                }
-                if (board[next] == 100) {
-                    return distances[100];
-                }
-            }
+        int[][] snakes = new int[m][2];
+        for (int i = 0; i < m; i++) {
+            line = br.readLine().split(" ");
+            snakes[i][0] = Integer.parseInt(line[0]);
+            snakes[i][1] = Integer.parseInt(line[1]);
         }
-        return -1;
-    }
 
+        System.out.println(solution(n, m, ladders, snakes));
+    }
 }
