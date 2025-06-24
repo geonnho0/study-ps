0d5701f86f72cb7ac8d417cdac6c01b2129366a8
Park Geonho <73037262+geonnho0@users.noreply.github.com>
2025-06-24 23:01:55 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 72 ms, Memory: 11776 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,56 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 
-    static int N, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-    static int[] arr, op;
+    private static int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        String[] line = br.readLine().split(" ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(line[i]);
-        }
-        line = br.readLine().split(" ");
-        op = new int[4];
-        for (int i = 0; i < 4; i++) {
-            op[i] = Integer.parseInt(line[i]);
-        }
-        dfs(arr[0], 1);
-        System.out.println(max);
-        System.out.print(min);
+    public static String solution(int n, int[] a, int[] count) {
+        dfs(a[0], 1, a, count);
+        return max + "\n" + min;
     }
 
-    static void dfs(int res, int index) {
-        if (index == N) {
-            max = Math.max(max, res);
-            min = Math.min(min, res);
+    private static void dfs(int sum, int curr, int[] a, int[] count) {
+        if (curr == a.length) {
+            min = Math.min(min, sum);
+            max = Math.max(max, sum);
             return;
         }
 
         for (int i = 0; i < 4; i++) {
-            if (op[i] == 0) {
-                continue;
-            }
-            op[i]--;
-            if (i == 0) {
-                dfs(res + arr[index], index + 1);
-            }
-            else if (i == 1) {
-                dfs(res - arr[index], index + 1);
-            }
-            else if (i == 2) {
-                dfs(res * arr[index], index + 1);
-            }
-            else {
-                dfs(res / arr[index], index + 1);
-            }
-            op[i]++;
+            if (count[i] == 0) continue;
+
+            count[i]--;
+          int newSum = sum;
+            if (i == 0) newSum += a[curr];
+            if (i == 1) newSum -= a[curr];
+            if (i == 2) newSum *= a[curr];
+            if (i == 3) newSum /= a[curr];
+            dfs(newSum, curr + 1, a, count);
+            count[i]++;
         }
     }
 
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        String[] arr = br.readLine().split(" ");
+        for (int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(arr[i]);
+        int[] count = new int[4];
+        arr = br.readLine().split(" ");
+        for (int i = 0; i < 4; i++)
+            count[i] = Integer.parseInt(arr[i]);
+        System.out.println(solution(n, a, count));
+    }
 }
